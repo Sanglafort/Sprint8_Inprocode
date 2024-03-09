c65c9a6f338fe18538a815225fc750d920025ab8
Sanglafort <silviaangfort@gmail.com>
2024-03-09 18:12:26 +0100

    Routes OK

--- frontend/sprint8/src/app/app.routes.ts
+++ frontend/sprint8/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { CalenderComponent } from './components/calender/calender.component';
+import { ChartComponent } from './components/chart/chart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Home', component: HomeComponent },
+  { path: 'home', title: 'Home', component: HomeComponent },
+  { path: 'map', title: 'Mapa', component: MapComponent },
+  { path: 'calender', title: 'Calendario', component: CalenderComponent },
+  { path: 'chart', title: 'Graficos', component: ChartComponent  },
+  { path: '**', redirectTo: '' },
+
+];
